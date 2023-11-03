a478916d3aecfb9ec99461dfaf58f18bd5ba18b1
ranuvijay <ranuvijay11@hotmail.com>
2023-11-03 14:10:38 +0530

    dar light theme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
 import { Provider } from "react-redux";
 import { useStore } from "./store/store.js";
-import MyRoutes from './myRoutes';
+import MyRoutes from "./myRoutes";
 import { BrowserRouter } from "react-router-dom";
 
 function App(props) {
