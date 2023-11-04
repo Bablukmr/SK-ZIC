a8b1089d276d04a62c5c12641ec3330b7df1fb36
Bablukmr <bablukmr800@gmail.com>
2023-11-04 16:58:04 +0530

    img..

--- src/users/layout/userFooter.js
+++ src/users/layout/userFooter.js
@@ -6,19 +6,19 @@ export default function WebFooter() {
   const darkMode = useSelector((state) => state.AuthReducer.darkMode);
   return (
     <div
-      className={`w-full  h-[700px] md:h-[400px] ${
+      className={`w-full py-6 h-[600px] md:h-[300px] lg:h-[330px] xl:h-[350px] ${
         darkMode ? "bg-slate-700" : "bg-[#383434]"
-      }   flex justify-center items-center`}
+      } flex justify-center items-center`}
     >
-      <div className="w-[80%] ml-[10%] md:ml-0 flex-col flex md:flex-row items-center justify-center ">
-        <div className="w-[90%] ml-[5%] md:w-[40%] h-[300px]">
+      <div className="w-[80%] ml-[10%] md:ml-0  flex flex-col md:flex-row items-center justify-center ">
+        <div className="w-[90%] md:w-[40%] h-[300px]  flex items-center justify-center">
           <img
             src="/sookee-thumbs-up.png"
             alt="/"
             className="h-[80%] md:h-[85%] lg:h-[95%] w-[80%]"
           />
         </div>
-        <div className="w-[90%] ml-[5%] md:w-[40%] h-[300px]">
+        <div className="w-[90%]  md:w-[40%] h-[300px] flex items-center justify-center">
           <img
             src="/Asset 12@2x-8.png"
             alt="/"
