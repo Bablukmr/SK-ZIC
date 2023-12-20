c3f8b1e5fb26875fa5f41ce4ecf386f0779d0b45
Bablukmr <bablukmr800@gmail.com>
2023-12-20 15:19:52 +0530

    nnnn

--- src/admin/layout/menu.js
+++ src/admin/layout/menu.js
@@ -1,38 +1,75 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { userLogout } from "../../store/action";
 // import axios from "axios";
 
 export default function AdminMenu(props) {
   const dispatch = useDispatch();
-
   const token = useSelector((state) => state.AuthReducer.token);
 
   const { setNavbarOpen, isMobile } = props;
+
+  const [selected, setSelected] = useState(0);
+  const [selectedsub, setSelectedsub] = useState(0);
+  // console.log("submenuOpen",submenuOpen);
+  const [submenuOpen, setSubmenuOpen] = useState(null);
+  // console.log("selectedsub",selectedsub);
+
+  const navigate = useNavigate();
   const [menuItems] = useState([
     { id: 1, value: "Dashboard", link: "/admin" },
-    { id: 2, value: "Manage User Account", link: "/admin/manage-users" },
-    { id: 3, value: "Manage Promotions", link: "/admin/manage-promotions" },
+    {
+      id: 2,
+      value: "Manage ",
+      submenu: [
+        { id: 3, value: "Manage User Account", link: "/admin/manage-users" },
+        {
+          id: 13,
+          value: "Manage Promotions",
+          link: "/admin/manage-promotions",
+        },
+      ],
+    },
     { id: 4, value: "Generate QR Codes", link: "/admin/generate-qr" },
     { id: 10, value: "Expired QR", link: "/admin/expired-qr" },
     { id: 11, value: "Verify Users", link: "/admin/verify-users" },
-    { id: 12, value: "Update Expired QR", link: "/admin/update-expired-qr" },
-    { id: 13, value: "Update Unused QR", link: "/admin/update-unused-qr" },
-
-    { id: 14, value: "upload-qr", link: "/admin/upload-qr" },
-
+    {
+      id: 12,
+      value: "Update QR",
+      submenu: [
+        {
+          id: 14,
+          value: "Update Expired QR",
+          link: "/admin/update-expired-qr",
+        },
+        { id: 15, value: "Update Unused QR", link: "/admin/update-unused-qr" },
+      ],
+    },
     { id: 6, value: "View Analytics", link: "#" },
     { id: 7, value: "Live Chat With Users", link: "/admin/admin-chat" },
-    { id: 9, value: "QR XLSX Download ", link: "/admin/download-qr" },
-    { id: 8, value: "Download QR Csv", link: "#" },
+    {
+      id: 9,
+      value: "Download",
+      submenu: [
+        { id: 16, value: "QR XLSX Download", link: "/admin/download-qr" },
+        { id: 17, value: "Download QR Csv", link: "#" },
+      ],
+    },
   ]);
-  const [selected, setSelected] = useState(1);
 
+  // const subMenuForUpdate = [
+  //   { id: 12, value: "Update Expired QR", link: "/admin/update-expired-qr" },
+  //   { id: 13, value: "Update Unused QR", link: "/admin/update-unused-qr" },
+  // ];
+  //bg-[#23262d]
   return (
-    <div className="w-[280px] text-white bg-[#23262d] h-[calc(100vh-50px)] overflow-auto">
-      <div className="flex items-center   h-[180px] flex-col">
+    <div
+      className={`w-[280px] text-white bg-[#23262d] ${
+        isMobile ? "h-[calc(100vh)] " : "h-[calc(100vh-50px)]"
+      }overflow-auto`}
+    >
+      <div className="flex items-center h-[180px] flex-col">
         {isMobile && (
           <div className="h-[40px] items-center w-full flex justify-end">
             <svg
@@ -58,41 +95,101 @@ export default function AdminMenu(props) {
         <div className="bg-white mt-8 rounded-full w-[100px] h-[100px] p-1">
           <img
             src="/2df2.jpg"
+            alt="/"
             className="rounded-full object-cover h-[100px] w-[100px]"
           />
         </div>
 
-        <p className="m-0 p-0 mt-3"> Jerry Maguire</p>
+        <p className="m-0 p-0 mt-3">Jerry Maguire</p>
       </div>
 
-      <ul className="list-none m-0 p-0 mt-10 md:mt-6 pb-6 space-y-2 mx-2">
-        {menuItems.map((d) => {
-          return (
-            <div key={d.id}>
-              <Link to={d.link} className="no-underline text-white">
-                <li
-                  className={`rounded pl-6 py-1 cursor-pointer  ${
-                    selected === d.id &&
-                    "bg-gradient-to-r from-[#8c8c8c] to-[#5C5C5C]"
-                  }`}
-                  key={d.id}
-                  onClick={() => {
-                    setSelected(d.id);
-                    if (d.id === 8) {
-                      window.open(
-                        "https://api-dev.skzicph.com/qr/generate-csv",
-                        "_blank"
-                      );
-                    }
-                    if (isMobile) setNavbarOpen(false);
-                  }}
-                >
-                  <small>{d.value}</small>
-                </li>
-              </Link>
-            </div>
-          );
-        })}
+      <ul className="list-none active:select-none m-0 p-0 mt-10 md:mt-6 pb-6 space-y-2 mx-2">
+        {menuItems.map((menuItem) => (
+          <div key={menuItem.id}>
+            <li
+              className={`pl-6 py-[7px] cursor-pointer rounded-md transition duration-300 ${
+                selected === menuItem.id &&
+                "bg-gradient-to-r from-[#8c8c8c] to-[#5C5C5C] "
+              }`}
+              onClick={() => {
+                setSelected(menuItem.id);
+                navigate(menuItem.link);
+                if (menuItem.submenu) {
+                  setSubmenuOpen(
+                    submenuOpen === menuItem.id ? null : menuItem.id
+                  );
+                } else {
+                  if (isMobile) setNavbarOpen(false);
+                }
+              }}
+            >
+              {/* <Link to={menuItem.link} className="no-underline text-white"> */}
+              <div className="flex items-center justify-between">
+                <small>{menuItem.value}</small>
+                {menuItem.submenu && (
+                  <div className="flex items-center pr-4 ml-auto">
+                    {submenuOpen === menuItem.id ? (
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-4">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+                    </svg>
+                    
+                    ) : (
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-4">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                    </svg>
+                    
+                    )}
+                  </div>
+                )}
+              </div>
+
+              {/* <small>{menuItem.value}</small>
+              {menuItem.submenu && (
+                <small className=" justify-end">
+                  {submenuOpen === menuItem.id ? <IoIosArrowUp /> : <IoIosArrowDown />}
+                </small>
+              )} */}
+              {/* </Link> */}
+            </li>
+
+            {/* subMenu */}
+            {menuItem.submenu && (
+              <ul
+                className={`list-none m-0 p-0 my-2 space-y-2 pl-2 transition duration-300 ${
+                  submenuOpen === menuItem.id
+                    ? "opacity-100"
+                    : "opacity-0 hidden"
+                }`}
+              >
+                {menuItem.submenu.map((submenuItem) => (
+                  <li
+                    key={submenuItem.id}
+                    className={`py-[7px] pl-6 rounded-md cursor-pointer transition duration-300 ${
+                      selectedsub === submenuItem.id &&
+                      "bg-gradient-to-r from-[#8c8c8c] to-[#5C5C5C]"
+                    } `}
+                    onClick={() => {
+                      navigate(submenuItem.link);
+                      setSelectedsub(submenuItem.id);
+                      if (isMobile) setNavbarOpen(false);
+                    }}
+                  >
+                    {/* <Link
+                      to={submenuItem.link}
+                      className="no-underline text-white"
+                      onClick={() => {
+                        setSelectedsub(submenuItem.id);
+                        if (isMobile) setNavbarOpen(false);
+                      }}
+                    > */}
+                    <small className="flex items-center ">{submenuItem.value}</small>
+                    {/* </Link> */}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
+        ))}
       </ul>
 
       {isMobile && (
